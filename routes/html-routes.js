9fe5a92f5ae87f24611aa8ed8f058610da647b59
Tyler <tylersweeney@gmail.com>
2018-02-01 19:24:05 -0800

    add html routes and schema

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,20 @@
 // Routes for each page we'll need
+
+//Dependencies
+var path = require("path");
+
+//Routes
+//========================================
+module.exports = function(app) {
+    app.get("/", function(req,res) {
+        res.sendFile(path.join(__dirname, "..public/index.html"));
+    });
+
+    app.get("/profile", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/profile1.html"));
+    });
+
+    app.get("/dashboard", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/dashboard.html"));
+    });
+};
