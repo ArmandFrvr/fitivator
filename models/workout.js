6ef016ac0338c9efc02e163d567ef2a7bed4da62
Alwyn Durham <alwyn.durham@evisions.com>
2018-02-01 16:30:55 -0800

    Fix broken links to js.  Draft js for profile.

---
+++ models/workout.js
@@ -0,0 +1,8 @@
+// Users have workouts
+// Workout fields:  user ID (FK ref. user), day (string), time (datetime)
+
+// when a user is deleted their workouts must be deleted, but not vice versa
+// probably don't need to handle deletes right now anyway but still
+
+// Workout model goes here
+
