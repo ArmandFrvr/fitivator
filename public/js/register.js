b01389e96a7ad070f5fde8bb4c804f8ccbf3143b
Alwyn Durham <alwyn.durham@evisions.com>
2018-02-09 08:23:01 -0800

    Separate sign up from log in form

--- public/js/register.js
+++ public/js/register.js
@@ -1,40 +1,79 @@
 
 $(document).ready(function() {
 
-
-  // Stupid stub function that puts a user in the db based on username
-  // Tyler-- replace this with amazon login code stuffs
+  // Basic login stuff
+  // Tyler-- replace this with Amazon login code (or add)
   $("#sign-up").on("click", function() {
 
     event.preventDefault();
     sessionStorage.clear();
 
     var user = $("#username").val().replace(/[^a-zA-Z0-9]+/g, '');
+    var email = $("#email").val().replace(/[^a-zA-Z@.-0-9]+/g, '');
     var password = $("#password").val();
 
     $.get("/api/users/" + user, function(data) {
-      alert("password is: " + password);
 
-      // If user doesn't exist, add them to the db (just username and email for now)
+      // If user doesn't exist, add them to the db
       if(!data) {
-        console.log("User not found");
-        let email = $("#email").val().replace(/[^a-zA-Z@.-0-9]+/g, '');
         addUser(user, email, password);
       }
       else {
+        alert("User " + user + " already exists.");
+        showLogin();
+      }
+    });
+  });
+
+  // Go to login screen
+  $("#log-in").on("click", function() {
+    showLogin();
+  });
+
+  // Go to register screen
+  $("#sign-up2").on("click", function() {
+    showRegister();
+  });
+
+  // Click Log In button on login screen
+  $("#log-in2").on("click", function() {
+
+    event.preventDefault();
+    sessionStorage.clear();
+
+    var user = $("#username2").val().replace(/[^a-zA-Z0-9]+/g, '');
+    var password = $("#password2").val();
+
+    $.get("/api/users/" + user, function(data) {
+
+      // If user doesn't exist
+      if(!data) {
+        alert("User " + user + " does not exist.");
+      }
+      else if(password != data.password) { // password doesn't match
+        // how tf do we do this without making the password visible in the api
+        alert("Incorrect password.");
+      }
+      else {
         // Log the user in
-        console.log("Logging user in: " + user);
         sessionStorage.setItem("username", user);
         // take existing user to their dashboard
         window.location.href = "/dashboard";
       }
     });
   });
-
-
-
 });
 
+
+function showLogin() {
+  $("#register").hide();
+  $("#login").show();
+}
+
+function showRegister() {
+  $("#login").hide();
+  $("#register").show();
+}
 
 
 // Adds user to db
@@ -54,5 +93,4 @@ function addUser(username, email, password) {
     sessionStorage.setItem("username", username);
     window.location.href = "/profile";
   });
-
 }
